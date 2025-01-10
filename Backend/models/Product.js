3ef2c893427dcef061f30fb5d50a3ca0cefb8239
rahul gurjar <rahulgurjar824582@gmail.com>
2025-01-11 00:02:46 +0530

    create product model and review model

---
+++ Backend/models/Product.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    comment: { 
+        type: String,
+        required: true 
+    },
+    rating :{
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
+    }
+}, { timestamps: true });
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+    },
+    reviews: [reviewSchema]
+})
