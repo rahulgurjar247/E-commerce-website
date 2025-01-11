85f3178ffa354b299e4183b4088c21d8f79e3a31
rahul gurjar <rahulgurjar824582@gmail.com>
2025-01-11 18:41:58 +0530

    routes setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom'
+import Navbar from './component/navbar'
+import { Home } from './pages/home'
+import SignIn from './pages/signin'
+import SignUp from './pages/signup'
+import { ProductDetail } from './pages/Productsdetail'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/signin" element={<SignIn/>} />
+          <Route path="/signup" element={<SignUp/>} />
+          <Route path='/product/:id' element={<ProductDetail/>} />
+        </Routes>
     </>
   )
 }
